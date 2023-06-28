9d8b7501ac8b34033b3fcca8325c8404b14915fd
UnknownDeveloptment <115506384+UnknownDeveloptment@users.noreply.github.com>
2023-06-28 12:15:56 +0300

    Delete Base64.lua

--- Base64.lua
+++ Base64.lua
@@ -8,7 +8,7 @@ local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 function enc(data)
     return ((data:gsub('.', function(x) 
         local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        fori=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
         return r;
     end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
         if (#x < 6) then return '' end
